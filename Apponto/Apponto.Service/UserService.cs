6679fe58f5663b1f55a3d9e4ad13d480a2f8cefa
José Luiz Albernaz Gomes Junior <junior.gomes@unear.net>
2015-05-11 14:30:29 -0300

    Revert "Implementado Entity Framework baseado em Code First (Deve-se modelar entidades desenvolvidas em bd);"
    This reverts commit ed9533e1f87d29a40858e1855f3fe336297cbf0a.

--- Apponto/Apponto.Service/UserService.cs
+++ Apponto/Apponto.Service/UserService.cs
@@ -1,4 +1,5 @@
-﻿using Apponto.Model;
+﻿using Apponto.Database;
+using Apponto.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +10,18 @@ namespace Apponto.Service
 {
     public class UserService
     {
+        private UserDatabase userDatabase;
+
         public UserService(String connectionString) {
+            userDatabase = new UserDatabase(connectionString);
         }
 
+        public bool Authenticate(String login, String password)
+        {
+            User user = userDatabase.GetUser(login);
+
+            //TODO: Tornar campo password criptografado.
+            return user.Password == password;
+        }
     }
 }
--- Apponto/Apponto/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Apponto.MvcApplication" Language="C#" %>
